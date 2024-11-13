8574e0b9c1b4fa0d113f76eba4760c9d3b131dd4
Daniel Guirra <daniel.guirra777@gmail.com>
2024-11-13 00:39:18 -0300

    init

---
+++ setup.py
@@ -0,0 +1,29 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((1280,720))
+
+pygame.display.set_caption("The Red Baron Game")
+
+icon=pygame.image.load("icon.png")
+
+pygame.display.set_icon(icon)
+
+clock=pygame.time.Clock()
+
+running=True
+
+while running:
+   for event in pygame.event.get():
+      if event.type==pygame.QUIT:
+         running=False
+
+
+   screen.fill("purple")
+
+   pygame.display.flip()
+
+   clock.tick(60)
+
+pygame.quit()
